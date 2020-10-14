77b9f35f0a25c5b36eaf03fc381a2c61b38bfb41
Юлия <72439922+13SavinaJulia@users.noreply.github.com>
2020-10-14 22:26:00 +0900

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = x * 2;
+                Console.WriteLine($"Результат = {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок Finally");
+            }
+            Console.WriteLine("Конец программы");
+            Console.ReadLine();
+        }
+    }
+}
